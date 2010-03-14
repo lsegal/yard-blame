e7e3639212981f18d44ebb24af8eadf8bf05bd16
Loren Segal <lsegal@soen.ca>
2010-03-14 09:13:10 -0400

    Add 1.8 compatibility

--- lib/yard-blame.rb
+++ lib/yard-blame.rb
@@ -1,25 +1,18 @@
-require 'yard'
-
-YARD::Templates::Engine.register_template_path(File.dirname(__FILE__) + '/../templates')
-
-class GitBlameHandler < YARD::Handlers::Ruby::MethodHandler
-  handles :def, :defs
-  
-  @@blame_files = {}
-  
+module GitBlameHandler
+  def self.blame_files; @@blame_files ||= {} end
   def register(obj) @object = obj; super end
   
   def process
     super
     info, bline = {}, statement.line_range.begin
-    unless content = @@blame_files[statement.file]
-      @@blame_files[statement.file] = 
-        `git blame -l #{statement.file}`.split("\n")
+    unless content = GitBlameHandler.blame_files[parser.file]
+      GitBlameHandler.blame_files[parser.file] = content =
+        `git blame -l #{parser.file}`.split("\n")
     end
     bline.upto(statement.line_range.end) do |index|
-      line = @@blame_files[statement.file][index-1]
+      line = content[index-1]
       if line =~ /^\^?(\S+)\s+\((.+?)\s+\d+/
-        info[index] = {rev: $1, name: $2}
+        info[index] = {:rev => $1, :name => $2}
       end
     end
     @object[:blame_info] = info
@@ -40,6 +33,7 @@ module GitBlameHelper
   end
 end
 
-module YARD::Templates::Helpers::MethodHelper
-  include GitBlameHelper
-end
+YARD::Templates::Engine.register_template_path(File.dirname(__FILE__) + '/../templates')
+YARD::Templates::Helpers::MethodHelper.send(:include, GitBlameHelper)
+YARD::Handlers::Ruby::MethodHandler.send(:include, GitBlameHandler)
+YARD::Handlers::Ruby::Legacy::MethodHandler.send(:include, GitBlameHandler)
